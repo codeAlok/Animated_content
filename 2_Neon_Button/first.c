678485ab6bcd6401dfa5fc795bf9f92057e026e9
alok <alok60541@gmail.com>
2022-06-25 22:09:48 +0530

    2_saved

---
+++ 2_Neon_Button/first.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+// #include<conio.h>
+ 
+// main function start from here
+
+void main(){
+    int a=10000000;
+    float b=5.0;
+    char d= '3';
+
+    /*
+
+    //printf("The value of a is %d \t %d\n",a,a);
+    printf("The value of b is %f\n",b);
+    printf("The value of d is %d\n",d);
+
+    printf("Enter the value of a ");
+    scanf("%d", &a);
+    
+    */
+    
+}
+
+
